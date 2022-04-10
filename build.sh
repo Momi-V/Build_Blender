bfbe9d3c04dd358193857e6a4c999ac2a0aa9b2a
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-04-10 16:33:08 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,20 +9,20 @@ mkdir ~/blender-git
 cd ~/blender-git
 git clone https://git.blender.org/blender.git
 
-mkdir log
 cd blender
 git pull origin master
 
-make update > ./log/update_1.log
+make update > ../log/update_1.log
 
 #apt install -y python3    #already installed in cloud instance
-#make update > ./log/update_2.log
 
-make deps > ./log/deps_1.log
+#make update > ../log/update_2.log
+
+make deps > ../log/deps_1.log
 
 apt install -y autoconf automake bison libtool tcl yasm meson ninja-build
 
-make deps > ./log/deps_2.log
-make deps -k > ./log/deps_3.log
-make deps -k > ./log/deps_4.log
-make deps > ./log/deps_5.log
+make deps > ../log/deps_2.log
+make deps -k > ../log/deps_3.log
+make deps -k > ../log/deps_4.log
+make deps > ../log/deps_5.log
