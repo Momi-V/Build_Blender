1ff8cd688f9bc4ceec1b5c896a28d0bb86905285
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-06-22 18:02:20 +0200

    Update buildRH.sh

--- buildRH.sh
+++ buildRH.sh
@@ -2,10 +2,10 @@
 
 dnf upgrade -y && dnf install -y epel-release && dnf config-manager --set-enabled crb
 
-sudo dnf install -y \
+dnf install -y \
 gcc gcc-c++ git subversion make cmake mesa-libGL-devel libX11-devel libXxf86vm-devel libXi-devel libXcursor-devel libXrandr-devel libXinerama-devel libstdc++-static
 
-sudo dnf install -y \
+dnf install -y \
 wayland-devel wayland-protocols-devel mesa-libEGL-devel libxkbcommon-devel dbus-devel kernel-headers
 
 mkdir ~/blender-git
