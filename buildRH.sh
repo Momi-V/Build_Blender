5ace5b3f5724d6d9fd2d4f3fe177a4213a8d1635
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-06-24 17:16:07 +0200

    Update buildRH.sh

--- buildRH.sh
+++ buildRH.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-dnf upgrade -y
-#&& dnf install -y epel-release 'dnf-command(config-manager)' && dnf config-manager --set-enabled crb
+dnf upgrade -y && dnf install -y epel-release 'dnf-command(config-manager)' && dnf config-manager --set-enabled crb
 
 dnf install -y \
 gcc gcc-c++ git subversion make cmake mesa-libGL-devel libX11-devel libXxf86vm-devel libXi-devel libXcursor-devel libXrandr-devel libXinerama-devel libstdc++-static
@@ -14,7 +13,6 @@ cd ~/blender-git
 git clone https://git.blender.org/blender.git
 
 cd blender
-git pull origin master
 
 dnf install -y \
 autoconf automake bison libtool yasm tcl meson ninja-build patch perl-FindBin alsa-lib-devel pulseaudio-libs-devel ncurses-devel diffutils python3-mako flex
