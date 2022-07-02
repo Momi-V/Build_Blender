6f09e944e0139e316656d86d6e1a7672cba8d470
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-07-02 11:24:34 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-dnf upgrade -y && dnf install -y epel-release 'dnf-command(config-manager)' && dnf config-manager --set-enabled powertools
+dnf upgrade -y && dnf install -y epel-release 'dnf-command(config-manager)' && dnf config-manager --set-enabled crb
 
 dnf install -y \
 gcc gcc-c++ git subversion make cmake mesa-libGL-devel libX11-devel libXxf86vm-devel libXi-devel libXcursor-devel libXrandr-devel libXinerama-devel libstdc++-static
