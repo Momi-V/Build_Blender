d1f916ede4f5d1bbce654c4d6cbf5d7a99fb6940
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-08-25 08:27:16 +0200

    New Packages

--- build.bash
+++ build.bash
@@ -2,11 +2,13 @@
 
 dnf upgrade -y && dnf install -y epel-release 'dnf-command(config-manager)' && dnf config-manager --set-enabled crb
 
-dnf install -y \
-gcc gcc-c++ git subversion make cmake mesa-libGL-devel libX11-devel libXxf86vm-devel libXi-devel libXcursor-devel libXrandr-devel libXinerama-devel libstdc++-static
+#dnf install -y xorg-x11-drivers
 
 dnf install -y \
-wayland-devel wayland-protocols-devel mesa-libEGL-devel libxkbcommon-devel dbus-devel kernel-headers
+gcc gcc-c++ git subversion make cmake mesa-libGL-devel mesa-libEGL-devel libX11-devel libXxf86vm-devel libXi-devel libXcursor-devel libXrandr-devel libXinerama-devel libstdc++-static
+
+dnf install -y \
+wayland-devel wayland-protocols-devel libxkbcommon-devel dbus-devel kernel-headers
 
 mkdir ~/blender-git
 cd ~/blender-git
