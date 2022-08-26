3dfce7f7f66baad998165cafaced1c658b453276
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-08-26 09:40:51 +0200

    Update buildRH.bash

--- buildRH.bash
+++ buildRH.bash
@@ -19,4 +19,5 @@ git checkout blender-v3.2-release
 dnf install -y \
 autoconf automake bison libtool yasm tcl meson ninja-build patch perl-FindBin diffutils alsa-lib-devel pulseaudio-libs-devel ncurses-devel flex python3-mako
 
-make deps -n | sed "s+cmake+cmake -DPACKAGE_USE_UPSTREAM_SOURCES=OFF+g" | bash
+make deps
+#make deps -n | sed "s+cmake+cmake -DPACKAGE_USE_UPSTREAM_SOURCES=OFF+g" | bash
