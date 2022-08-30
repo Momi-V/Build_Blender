0d0bcfff46e6d28e26867d6e496ecffd7ea47d74
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-08-30 16:05:40 +0200

    Update buildRH.bash

--- buildRH.bash
+++ buildRH.bash
@@ -15,6 +15,8 @@ cd blender
 git checkout blender-v3.2-release
 
 dnf install -y \
-autoconf automake bison libtool yasm tcl meson ninja-build patch perl-FindBin diffutils alsa-lib-devel pulseaudio-libs-devel ncurses-devel flex python3-mako
+autoconf automake bison libtool tcl yasm meson ninja-build
+dnf install -y \
+patch alsa-lib-devel pulseaudio-libs-devel ncurses-devel zlib-devel flex python3-mako
 
 make deps
