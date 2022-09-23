2448b97959efa7a1a5ee72002e33fccb241d76de
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-09-23 11:47:04 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -17,7 +17,7 @@ git checkout blender-v3.2-release
 mkdir ../log ../tar
 
 dnf install -y gcc-toolset-9
-scl enable gcc-toolset-9 "bash -e || true" << EOL
+scl enable gcc-toolset-9 "bash || true" << EOL
 shopt -s extglob
 
 make deps |& tee ../log/0_deps.txt
